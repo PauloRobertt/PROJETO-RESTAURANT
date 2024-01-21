dffd65b61056c7857d454b126243a91e5241219a
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-21 17:50:52 -0300

    Estilização do componente Topo

--- cardapio-restaurante/src/componentes/Topo/index.jsx
+++ cardapio-restaurante/src/componentes/Topo/index.jsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import styles from './Topo.module.css';
-import img from "../../../public/assets/banner.png";
 
 export default function topo(){
     return(
         <header className={styles.topo}>
             <h1>RESTAURANT</h1>
-            <img src={img} alt="banner"/>
             <p>
                 De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados
             </p>
