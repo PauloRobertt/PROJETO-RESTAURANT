53aaeb3188fd3c4e62a64c768df7faa16e0f2fd0
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-21 17:51:25 -0300

    Modificação na estrutura do App.js

--- cardapio-restaurante/src/pages/_app.js
+++ cardapio-restaurante/src/pages/_app.js
@@ -5,11 +5,16 @@ import CampoBusca from '../componentes/CampoDeBusca';
 import Cardapio from '../componentes/Cardapio';
 //import {produtos} from '../data';
 
+import styles from '../styles/Home.module.css';
+import '../styles/globals.css';
+
 export default function Restaurante(){
   return(
-    <div>
-      <Topo/>
-      <CampoBusca/>
+    <div className={styles.restaurante}>
+      <div className={styles.sectionLimitada}>
+        <Topo/>
+        <CampoBusca/>
+      </div>
     </div>
   )
 }
