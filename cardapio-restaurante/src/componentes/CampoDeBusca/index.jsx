918437809d194c1bdc9cb2168a8495dc2bf30742
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-24 20:59:47 -0300

    Movendo a pasta img para dentro da src

--- cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
+++ cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
@@ -1,13 +1,16 @@
+import { FiltroCategoria } from '../../servico';
+
 export default function CampoBusca(){
+
     return(
         <div>
             <div>
-                <button>Entradas</button>
-                <button>Massas</button>
-                <button>Carnes</button>
-                <button>Bebidas</button>
-                <button>Saladas</button>
-                <button>Sobremesas</button>
+                <button onClick={() => FiltroCategoria('Entradas')}>Entradas</button>
+                <button onClick={() => FiltroCategoria('Massas')}>Massas</button>
+                <button onClick={() => FiltroCategoria('Carnes')}>Carnes</button>
+                <button onClick={() => FiltroCategoria('Bebidas')}>Bebidas</button>
+                <button onClick={() => FiltroCategoria('Saladas')}>Saladas</button>
+                <button onClick={() => FiltroCategoria('Sobremesas')}>Sobremesas</button>
             </div>
             <input
                 type="text"
--- cardapio-restaurante/src/componentes/Cards/index.jsx
+++ cardapio-restaurante/src/componentes/Cards/index.jsx
@@ -1,14 +1,17 @@
-export const Cardapio = ({nome,imagem,categoria,preco,descricao}) => {
+
+
+const Cards = ({nome, categoria, preco, descricao, imagem}) => {
     return(
         <div>
             <figure>
-                <img src={imagem} alt="Produto"/>
+                <img src={imagem} alt={nome}/>
             </figure>
             <h2>{nome}</h2>
             <p>{categoria}</p>
             <p>{descricao}</p>
             <p>{preco}</p>
-
         </div>
     )
 }
+
+export default Cards;
