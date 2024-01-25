53436abe773c418af9461de2b83cba936738a1a8
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-24 21:00:16 -0300

    Modificação na estrutura do App.js

--- cardapio-restaurante/src/pages/_app.js
+++ cardapio-restaurante/src/pages/_app.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-import Topo from '../componentes/Topo';
+import Topo from '../componentes/Header';
 import CampoBusca from '../componentes/CampoDeBusca';
-import Cardapio from '../componentes/Cardapio';
-//import {produtos} from '../data';
+import Cards from '../componentes/Cards';
+
+import { RetornaProduto, FiltroCategoria } from '../servico';
 
 import styles from '../styles/Home.module.css';
 import '../styles/globals.css';
 
-export default function Restaurante(){
+const Restaurante = () => {
+
+  const [listaProdutos, setListaProdutos] = useState(RetornaProduto);
+
   return(
     <div className={styles.restaurante}>
       <div className={styles.sectionLimitada}>
         <Topo/>
         <CampoBusca/>
+        {
+          listaProdutos.map((jogo) => (
+            <Cards
+              key={jogo.id}
+              nome={jogo.nome}
+              categoria={jogo.categoria}
+              preco={jogo.preco}
+              descricao={jogo.descricao}
+              imagem={jogo.imagem} 
+            />
+          ))
+        }
       </div>
     </div>
   )
 }
+
+export default Restaurante;
