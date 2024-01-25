e4a1a93363e848731e8e3fab846128be8b103beb
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-25 17:10:07 -0300

    Modificação dos botões

--- cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
+++ cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
@@ -1,21 +1,26 @@
-import { FiltroCategoria } from '../../servico';
+import styles from './CampoDeBusca.module.css';
 
-export default function CampoBusca(){
+const CampoBusca =({onEntradasCLick, onMassaClick, onCarnesClick, onBebidasClick, onSaladasClick, onSobremesasClick, onBuscaChange, buscaProduto}) =>{
 
     return(
-        <div>
-            <div>
-                <button onClick={() => FiltroCategoria('Entradas')}>Entradas</button>
-                <button onClick={() => FiltroCategoria('Massas')}>Massas</button>
-                <button onClick={() => FiltroCategoria('Carnes')}>Carnes</button>
-                <button onClick={() => FiltroCategoria('Bebidas')}>Bebidas</button>
-                <button onClick={() => FiltroCategoria('Saladas')}>Saladas</button>
-                <button onClick={() => FiltroCategoria('Sobremesas')}>Sobremesas</button>
+        <div className={styles.sectionCampoDeBusca}>
+            <div className={styles.sectionButton}>
+                <button onClick={onEntradasCLick}><img src="/assets/entrada.png" alt="Entradas"/>Entradas</button>
+                <button onClick={onMassaClick}><img src="/assets/massa.png" alt="Massas"/>Massas</button>
+                <button onClick={onCarnesClick}><img src="/assets/carne.png" alt="Carnes"/>Carnes</button>
+                <button onClick={onBebidasClick}><img src="/assets/bebidas.png" alt="Bebidas"/>Bebidas</button>
+                <button onClick={onSaladasClick}><img src="/assets/salada.png" alt="Saladas"/>Saladas</button>
+                <button onClick={onSobremesasClick}><img src="/assets/sobremesa.png" alt="Sobremesas"/>Sobremesas</button>
             </div>
             <input
+                id='input'
                 type="text"
+                value={buscaProduto}
+                onChange={(event) => onBuscaChange(event.target.value)}
                 placeholder="Pesquise aqui um dos pratos do nosso cardápio"
             />
         </div>
     )
 }
+
+export default CampoBusca;
