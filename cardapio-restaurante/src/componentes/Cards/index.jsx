dc6a58b6a6f02ae0dea03a1d8b86c67dfa0ea19e
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-26 00:13:44 -0300

    Mudança na estruturação no componente Cards

--- cardapio-restaurante/src/componentes/Cards/index.jsx
+++ cardapio-restaurante/src/componentes/Cards/index.jsx
@@ -1,15 +1,18 @@
-
+import style from './Cards.module.css';
+import Image from 'next/image';
 
 const Cards = ({nome, categoria, preco, descricao, imagem}) => {
     return(
-        <div>
+        <div className={style.Card}>
             <figure>
-                <img src={imagem} alt={nome}/>
+                <Image src={imagem} alt={nome}/>
             </figure>
-            <h2>{nome}</h2>
-            <p>{categoria}</p>
-            <p>{descricao}</p>
-            <p>{preco}</p>
+            <div className={style.info}>
+                <h3>{nome}</h3>
+                <small>{categoria}</small>
+                <p>{descricao}</p>
+                <p className={style.preco}>{preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})}</p>
+            </div>
         </div>
     )
 }
