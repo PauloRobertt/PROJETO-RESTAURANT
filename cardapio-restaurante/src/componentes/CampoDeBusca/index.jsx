262b491914ea366caed5d00d0d8c87bf082a7aa6
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-26 21:42:56 -0300

    Troca de Cor
    Quando o botão for selecionado ele ira mudar de cor

--- cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
+++ cardapio-restaurante/src/componentes/CampoDeBusca/index.jsx
@@ -1,16 +1,16 @@
 import styles from './CampoDeBusca.module.css';
 
-const CampoBusca =({onEntradasCLick, onMassaClick, onCarnesClick, onBebidasClick, onSaladasClick, onSobremesasClick, onBuscaChange, buscaProduto}) =>{
+const CampoBusca =({onEntradasCLick, onMassaClick, onCarnesClick, onBebidasClick, onSaladasClick, onSobremesasClick, onBuscaChange, buscaProduto, botao}) =>{
 
     return(
         <div className={styles.sectionCampoDeBusca}>
             <div className={styles.sectionButton}>
-                <button onClick={onEntradasCLick}><img src="/assets/entrada.png" alt="Entradas"/>Entradas</button>
-                <button onClick={onMassaClick}><img src="/assets/massa.png" alt="Massas"/>Massas</button>
-                <button onClick={onCarnesClick}><img src="/assets/carne.png" alt="Carnes"/>Carnes</button>
-                <button onClick={onBebidasClick}><img src="/assets/bebidas.png" alt="Bebidas"/>Bebidas</button>
-                <button onClick={onSaladasClick}><img src="/assets/salada.png" alt="Saladas"/>Saladas</button>
-                <button onClick={onSobremesasClick}><img src="/assets/sobremesa.png" alt="Sobremesas"/>Sobremesas</button>
+                <button className={botao === 'Entradas' ? styles.botaoApertado : styles.botaoNormal} onClick={onEntradasCLick}><img src="/assets/entrada.png" alt="Entradas"/>Entradas</button>
+                <button className={botao === 'Massas' ? styles.botaoApertado : styles.botaoNormal} onClick={onMassaClick}><img src="/assets/massa.png" alt="Massas"/>Massas</button>
+                <button className={botao === 'Carnes' ? styles.botaoApertado : styles.botaoNormal} onClick={onCarnesClick}><img src="/assets/carne.png" alt="Carnes"/>Carnes</button>
+                <button className={botao === 'Bebidas' ? styles.botaoApertado :styles.botaoNormal} onClick={onBebidasClick}><img src="/assets/bebidas.png" alt="Bebidas"/>Bebidas</button>
+                <button className={botao === 'Saladas' ? styles.botaoApertado : styles.botaoNormal} onClick={onSaladasClick}><img src="/assets/salada.png" alt="Saladas"/>Saladas</button>
+                <button className={botao === 'Sobremesas' ? styles.botaoApertado : styles.botaoNormal} onClick={onSobremesasClick}><img src="/assets/sobremesa.png" alt="Sobremesas"/>Sobremesas</button>
             </div>
             <input
                 id='input'
