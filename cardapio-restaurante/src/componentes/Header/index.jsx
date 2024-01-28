825033d1f4721c8f4239bc98f4e45d8962ae7718
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-01-28 02:27:46 -0300

    Adicionamento de div
    Adicionamento de uma div para guardar os elementos do componente Topo

--- cardapio-restaurante/src/componentes/Header/index.jsx
+++ cardapio-restaurante/src/componentes/Header/index.jsx
@@ -4,10 +4,12 @@ import styles from './Topo.module.css';
 export default function topo(){
     return(
         <header className={styles.topo}>
-            <h1>RESTAURANT</h1>
-            <p>
-                De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados
-            </p>
+            <div className={styles.info}>
+                <h1>RESTAURANT</h1>
+                <p>
+                    De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados
+                </p>
+            </div>
         </header>
     )
 }
